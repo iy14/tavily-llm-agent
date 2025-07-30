45e66563324e62005e71fd42114c15a15ad64bb7
itamaryacoby <itamar@tastewise.io>
2025-07-31 01:06:29 +0300

    tools __init__

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,12 @@
 # Tools package
 from .tavily import tavily_search
 from .openai_summarizer import summarize_content, select_top_points
-from .cache import get_cached_newsletter, cache_newsletter, get_cache_stats
+from .cache import (
+    get_cached_newsletter,
+    cache_newsletter,
+    get_cache_stats,
+    get_cache_key,
+    clear_cache_for_profession,
+)
+from .profession_validator import validate_profession, validate_and_correct_profession
+from .url_extractor import extract_urls_from_newsletter, get_detailed_explanation
